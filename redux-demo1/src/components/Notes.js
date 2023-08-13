d9f29878528f5fef516679f8f1ae49a3f93709c5
duonganh <duonganh2973@gmail.com>
2023-08-13 21:49:34 +0700

    refactor

--- redux-demo1/src/components/Notes.js
+++ redux-demo1/src/components/Notes.js
@@ -11,11 +11,22 @@ const Note = ({ note, handleClick }) => {
 
 const Notes = () => {
   const dispatch = useDispatch();
-  const notes = useSelector((state) => state);
+  const notes = useSelector((state) => state.notes);
+  // read the current value of the filter from the store
+  const filter = useSelector((state) => state.filter);
+  let notesToShow;
+  switch (filter) {
+    case "IMPORTANT":
+       notesToShow = notes.filter((note) => note.important); break;
+    case "NONIMPORTANT":
+       notesToShow = notes.filter((note) => !note.important); break;
+    default:
+        notesToShow = notes; break;
+  }
 
   return (
     <ul>
-      {notes.map((note) => (
+      {notesToShow.map((note) => (
         <Note
           key={note.id}
           note={note}
--- redux-demo1/src/index.js
+++ redux-demo1/src/index.js
@@ -1,13 +1,25 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import { createStore } from "redux";
+import {configureStore} from '@reduxjs/toolkit';
 
 import { Provider } from "react-redux";
 
 import App from "./App";
 import noteReducer from "./reducers/noteReducer";
+import filterReducer from "./reducers/filterReducer";
 
-const store = createStore(noteReducer);
+// instead of createStore, combineReducers
+const store=configureStore({
+  reducer: {
+    notes: noteReducer,
+    filter: filterReducer
+  }
+});
+
+store.subscribe(() => console.log(store.getState()))
+
+// store.dispatch(filterChange('IMPORTANT'))
+// store.dispatch(createNote('combineReducers forms one reducer from many simple reducers'))
 
 ReactDOM.createRoot(document.getElementById("root")).render(
   <Provider store={store}>
